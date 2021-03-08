799334c23e393c230d2531d770382be4530095d9
yash20010 <throne.yash@gmail.com>
2021-03-07 22:17:19 -0700

    Adding more katas

---
+++ challenge310.js
@@ -0,0 +1,10 @@
+// Complete the function that takes one argument, a list of words, and returns the length of the longest word in the list.
+
+// For example:
+
+// ['simple', 'is', 'better', 'than', 'complex'] ==> 7
+// Do not modify the input list.
+
+function longest(words) {
+  return Math.max(...words.map(w => w.length))
+}
