fce1d21c58346c897fa95193a8c5eee0eb7f6138
yash20010 <throne.yash@gmail.com>
2021-03-07 22:16:41 -0700

    Adding more katas

---
+++ challenge309.js
@@ -0,0 +1,10 @@
+// Task
+// Your challenge is to write a function named getSlope/get_slope/GetSlope that calculates the slope of the line through two points.
+
+// Input
+// Each point that the function takes in is an array 2 elements long. The first number is the x coordinate and the second number is the y coordinate. If the line through the two points is vertical or if the same point is given twice, the function should return null/None.
+
+function getSlope(p1, p2) {
+  if(p1[0] === p2[0])  return null
+  return (p1[1]-p2[1])/(p1[0]-p2[0])
+}
