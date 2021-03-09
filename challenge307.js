bc4d9b536067105ed35e39fc3618b98e118f9574
yash20010 <throne.yash@gmail.com>
2021-03-08 22:15:07 -0700

    Adding more katas

---
+++ challenge307.js
@@ -0,0 +1,12 @@
+// In this kata, you will do addition and subtraction on a given string. The return value must be also a string.
+
+// Note: the input will not be empty.
+
+// Examples
+// "1plus2plus3plus4"  --> "10"
+// "1plus2plus3minus4" -->  "2"
+
+function calculate(str) {
+  //your code here...
+  return ''+ eval(str.replace(/plus/g,'+').replace(/minus/g,'-'))
+  }
