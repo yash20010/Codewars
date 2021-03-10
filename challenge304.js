ba7cac49fb5c197bfb2a5c948fe182bd0e8c2dc4
yash20010 <throne.yash@gmail.com>
2021-03-09 22:13:01 -0700

    Adding more katas

---
+++ challenge304.js
@@ -0,0 +1,15 @@
+// Bob is a lazy man.
+
+// He needs you to create a method that can determine how many letters and digits are in a given string.
+
+// Example:
+
+// "hel2!lo" --> 6
+
+// "wicked .. !" --> 6
+
+// "!?..A" --> 1
+
+function countLettersAndDigits(input) {
+  return input.replace(/[^a-z0-9]/gi,'').length
+}
