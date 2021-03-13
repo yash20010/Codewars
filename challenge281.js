20b55155a2d0fa1fb051884c5f4c7bc6a7875451
yash20010 <throne.yash@gmail.com>
2021-03-12 22:15:45 -0700

    Adding more katas

---
+++ challenge281.js
@@ -0,0 +1,11 @@
+// In this Kata, you will be given a string and your task will be to return a list of ints detailing the count of uppercase letters, lowercase, numbers and special characters, as follows.
+
+// Solve("*'&ABCDabcde12345") = [4,5,5,3]. 
+// --the order is: uppercase letters, lowercase, numbers and special characters.
+// More examples in the test cases.
+
+// Good luck!
+
+function solve(s){
+  return [s.replace(/[^A-Z]/g, '').length, s.replace(/[^a-z]/g, '').length, s.replace(/[^0-9]/g, '').length, s.replace(/[A-z0-9]/g, '').length]
+}
