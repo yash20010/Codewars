8d03ccd52707cf6bd090224e05925ae51086978d
yash20010 <throne.yash@gmail.com>
2021-03-13 23:12:44 -0700

    Adding more katas

---
+++ challenge142.js
@@ -0,0 +1,10 @@
+// Create a method that accepts an array of names, and returns an array of each name with its first letter capitalized.
+
+// example
+
+// capMe(['jo', 'nelson', 'jurie'])     // returns ['Jo', 'Nelson', 'Jurie']
+// capMe(['KARLY', 'DANIEL', 'KELSEY']) // returns ['Karly', 'Daniel', 'Kelsey']
+
+function capMe(names) {
+  return names.map(n => n.slice(0,1).toUpperCase() + n.slice(1).toLowerCase())
+}
