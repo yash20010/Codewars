c2123911372d1fe5f7ab7ef4428a283492e31feb
yash20010 <throne.yash@gmail.com>
2021-03-14 23:04:08 -0600

    Adding more katas

---
+++ challenge160.js
@@ -0,0 +1,23 @@
+// Linked lists are data structures composed of nested or chained objects, each containing a single value and a reference to the next object.
+
+// Here's an example of a list:
+
+// {value: 1, next: {value: 2, next: {value: 3, next: null}}}
+// Write a function listToArray (or list_to_array in Python) that converts a list to an array, like this:
+
+// [1, 2, 3]
+// Assume all inputs are valid lists with at least one value. For the purpose of simplicity, all values will be either numbers, strings, or Booleans.
+
+let arr = []
+
+function listToArray(obj) {
+  //write your code here
+  arr.push(obj.value)
+  if(obj.next !== null){
+    return listToArray(obj.next)
+  }else{
+    let x = arr.slice()
+    arr = []
+    return x
+  }
+}
