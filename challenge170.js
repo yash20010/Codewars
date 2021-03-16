fa5cc3a3550ac7baefdd2e60a99fd89d2c2982bd
yash20010 <throne.yash@gmail.com>
2021-03-16 01:07:12 -0600

    Adding more katas

---
+++ challenge170.js
@@ -0,0 +1,11 @@
+// Write a function named numbers that returns true if all the parameters it is passed are of the Number type. Otherwise, the function should return false. The function should accept any number of parameters.
+
+// Example usage:
+
+// numbers(1, 4, 3, 2, 5); // true
+// numbers(1, "a", 3); // false
+// numbers(1, 3, NaN); // true
+
+function numbers(...args){
+  return args.every(n => typeof n === 'number')
+}
