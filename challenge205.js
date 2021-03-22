b6c558ba29082dbd88954dab34f15600c55546f8
yash20010 <throne.yash@gmail.com>
2021-03-21 23:50:10 -0600

    Adding more katas

---
+++ challenge205.js
@@ -0,0 +1,38 @@
+// You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+// Your task is to return an object (associative array in PHP) which includes the count of each coding language represented at the meetup.
+
+// For example, given the following input array:
+
+// var list1 = [
+//   { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'C' },
+//   { firstName: 'Anna', lastName: 'R.', country: 'Liechtenstein', continent: 'Europe', age: 52, language: 'JavaScript' },
+//   { firstName: 'Ramon', lastName: 'R.', country: 'Paraguay', continent: 'Americas', age: 29, language: 'Ruby' },
+//   { firstName: 'George', lastName: 'B.', country: 'England', continent: 'Europe', age: 81, language: 'C' },
+// ];
+// your function should return the following object (associative array in PHP):
+
+// { C: 2, JavaScript: 1, Ruby: 1 }
+// Notes:
+
+// The order of the languages in the object does not matter.
+// The count value should be a valid number.
+// The input array will always be valid and formatted as in the example above.
+
+
+
+
+// This kata is part of the Coding Meetup series which includes a number of short and easy to follow katas which have been designed to allow mastering the use of higher-order functions. In JavaScript this includes methods like: forEach, filter, map, reduce, some, every, find, findIndex. Other approaches to solving the katas are of course possible.
+
+// Here is the full list of the katas in the Coding Meetup series:
+
+function countLanguages(list) {
+  // thank you for checking out the Coding Meetup kata :)
+  return list.reduce((obj, item) => {
+    if(!(item.language in obj)){
+      obj[item.language] = 0
+    }
+    obj[item.language]++
+    return obj
+  } , {})
+}
