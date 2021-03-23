56f44f1e436fe27413a2906be4b6d4e52a40cf3f
yash20010 <throne.yash@gmail.com>
2021-03-23 14:49:57 -0600

    Adding more katas

---
+++ challenge216.js
@@ -0,0 +1,15 @@
+// Write function replaceAll (Python: replace_all) that will replace all occurrences of an item with another.
+
+// Python / JavaScript: The function has to work for strings and lists.
+
+// Example: replaceAll [1,2,2] 1 2 -> in list [1,2,2] we replace 1 with 2 to get new list [2,2,2]
+
+// replaceAll(replaceAll(array: [1,2,2], old: 1, new: 2) // [2,2,2]
+
+function replaceAll(seq, find, replace) {
+  if(Array.isArray(seq)){
+    return seq.map(n => n===find ? replace : n)
+  }else{
+    return seq.split('').map(n => n===find ? replace : n).join(''); 
+  }
+}
