75881c58523bb4ad15159172c80a75073879dcfe
yash20010 <throne.yash@gmail.com>
2021-04-01 21:40:54 -0600

    Adding more katas

---
+++ challenge250.js
@@ -0,0 +1,12 @@
+// Given a number, write a function to output its reverse digits. (e.g. given 123 the answer is 321)
+
+// Numbers should preserve their sign; i.e. a negative number should still be negative when reversed.
+
+// Examples
+//  123 ->  321
+// -456 -> -654
+// 1000 ->    1
+
+function reverseNumber(n) {
+  return n>0 ? +String(n).split('').reverse().join('') : +('-' + String(Math.abs(n)).split('').reverse().join(''))
+}
