64c41b891f9fd05c9e706e34a06b24cb2cfdcda3
yash20010 <throne.yash@gmail.com>
2021-04-05 22:07:23 -0600

    Adding more katas

---
+++ challenge298.js
@@ -0,0 +1,26 @@
+// In this kata, we're going to create the function nato that takes a word and returns a string that spells the word using the NATO phonetic alphabet.
+
+// There should be a space between each word in the returned string, and the first letter of each word should be capitalized.
+
+// For those of you that don't want your fingers to bleed, this kata already has a dictionary typed out for you.
+
+// nato('hi') // --> 'Hotel India'
+// nato('abc') // --> 'Alpha Bravo Charlie'
+
+var nato = (function() {
+  var letters =  {
+    "A": "Alpha",  "B": "Bravo",   "C": "Charlie",
+    "D": "Delta",  "E": "Echo",    "F": "Foxtrot",
+    "G": "Golf",   "H": "Hotel",   "I": "India",
+    "J": "Juliett","K": "Kilo",    "L": "Lima",
+    "M": "Mike",   "N": "November","O": "Oscar",
+    "P": "Papa",   "Q": "Quebec",  "R": "Romeo",
+    "S": "Sierra", "T": "Tango",   "U": "Uniform",
+    "V": "Victor", "W": "Whiskey", "X": "X-ray",
+    "Y": "Yankee", "Z": "Zulu"
+  }
+  
+  return function(word) {
+    return word.split('').map(l => letters[l.toUpperCase()]).join(' ')
+  }
+})()
