d6f46b05a4414302575b524c88db5eb59bd12d39
throne.yash@gmail.com <throne.yash@gmail.com>
2021-04-08 21:48:07 -0600

    Adding more katas

--- challenge310.js
+++ challenge310.js
@@ -1,10 +1,14 @@
-// Complete the function that takes one argument, a list of words, and returns the length of the longest word in the list.
+// Create a function isDivisible(n,...) that checks if the first argument n is divisible by all other arguments (return true if no other arguments)
 
-// For example:
+// Example:
 
-// ['simple', 'is', 'better', 'than', 'complex'] ==> 7
-// Do not modify the input list.
+// isDivisible(6,1,3)--> true because 6 is divisible by 1 and 3
+// isDivisible(12,2)--> true because 12 is divisible by 2
+// isDivisible(100,5,4,10,25,20)--> true
+// isDivisible(12,7)--> false because 12 is not divisible by 7
+// This kata is following kata: http://www.codewars.com/kata/is-n-divisible-by-x-and-y
 
-function longest(words) {
-  return Math.max(...words.map(w => w.length))
+function isDivisible(...arr){
+  let n = arr.shift()
+  return !(arr.some(d => n%d !== 0))
 }
