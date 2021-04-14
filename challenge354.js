c87cfa16fde6f8b9799601a160956e6a99e8edfd
throne.yash@gmail.com <throne.yash@gmail.com>
2021-04-14 09:14:25 -0600

    Adding more Katas

---
+++ challenge354.js
@@ -0,0 +1,11 @@
+// Write a function that appends the items from sequence 2 onto sequence 1, returning the newly formed sequence. Your function should also be able to handle nested sequences.
+
+// For example:
+
+// ['a','b','c'], [1,2,3]     --> ['a','b','c',1,2,3]
+// [['x','x'],'B'], ['c','D'] --> [['x','x'],'B','c','D']
+
+function appendArrays(arr1, arr2) {
+  arr1.push(...arr2);
+  return arr1;
+}
