05e6bc46add30ab0acc41c4747e8dcc327027eac
throne.yash@gmail.com <throne.yash@gmail.com>
2021-04-19 12:07:49 -0600

    Adding more katas

---
+++ challenge368.js
@@ -0,0 +1,19 @@
+// You have to create a method "compoundArray" which should take as input two int arrays of different length and return one int array with numbers of both arrays shuffled one by one.
+
+// Input - {1,2,3,4,5,6} and {9,8,7,6}
+// Output - {1,9,2,8,3,7,4,6,5,6}
+
+function compoundArray(a, b) {
+  let arr = [];
+  for (let i = 0; i < Math.max(a.length, b.length); i++) {
+    if (a.length > i && b.length > i) {
+      arr.push(a[i]);
+      arr.push(b[i]);
+    } else if (a.length <= i && b.length > i) {
+      arr.push(b[i]);
+    } else {
+      arr.push(a[i]);
+    }
+  }
+  return arr;
+}
