7e1057faa8bc9d0dcd32afaecc8fe30e4934e9ef
throne.yash@gmail.com <throne.yash@gmail.com>
2021-05-08 18:33:35 -0600

    Adding more katas

---
+++ challenge403.js
@@ -0,0 +1,10 @@
+// Find the area of a rectangle when provided with one diagonal and one side of the rectangle. If the input diagonal is less than or equal to the length of the side, return "Not a rectangle". If the resultant area has decimals round it to two places.
+
+// This kata is meant for beginners. Rank and upvote to bring it out of beta!
+
+function area(d, l) {
+  //Write your own Code!
+  return d <= l
+    ? "Not a rectangle"
+    : Math.round(l * Math.sqrt(d ** 2 - l ** 2) * 100) / 100;
+}
