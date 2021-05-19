b189052f87ad0368d70d891a112ed711e53bd287
throne.yash@gmail.com <throne.yash@gmail.com>
2021-05-19 16:02:37 -0600

    Adding more katas

---
+++ challenge431.js
@@ -0,0 +1,11 @@
+// In this Kata, you will be given a string that has lowercase letters and numbers. Your task is to compare the number groupings and return the largest number. Numbers will not have leading zeros.
+
+// For example, solve("gh12cdy695m1") = 695, because this is the largest of all number groupings.
+
+// Good luck!
+
+// Please also try Simple remove duplicates
+
+function solve(s) {
+  return Math.max(...s.split(/[^\d]/g));
+}
