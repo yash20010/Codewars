ecbf1d991300b91f54da7d05b3ab9765a3d08c28
throne.yash@gmail.com <throne.yash@gmail.com>
2021-05-21 16:01:45 -0600

    Adding more katas

---
+++ challenge430.js
@@ -0,0 +1,23 @@
+// The Arara are an isolated tribe found in the Amazon who count in pairs. For example one to eight is as follows:
+
+// 1 = anane
+// 2 = adak
+// 3 = adak anane
+// 4 = adak adak
+// 5 = adak adak anane
+// 6 = adak adak adak
+// 7 = adak adak adak anane
+// 8 = adak adak adak adak
+
+// Take a given number and return the Arara's equivalent.
+
+// e.g.
+
+// countArara(3) 'adak anane'
+
+// countArara(8) 'adak adak adak adak'
+// https://en.wikipedia.org/wiki/Arara_people
+
+function countArara(n) {
+  return `${"adak ".repeat(Math.floor(n / 2))}${"anane".repeat(n % 2)}`.trim();
+}
