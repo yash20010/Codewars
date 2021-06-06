6dd4ee00e2c17fa4a715bf5aefb054166b517f55
throne.yash@gmail.com <throne.yash@gmail.com>
2021-06-06 12:59:35 -0600

    Adding more katas

---
+++ challenge477.js
@@ -0,0 +1,10 @@
+// Imagine there's a big cube consisting of n3 n^3n
+// 3
+//   small cubes. Calculate, how many small cubes are not visible from outside.
+
+// For example, if we have a cube which has 4 cubes in a row, then the function should return 8, because there are 8 cubes inside our cube (2 cubes in each dimension)
+
+function notVisibleCubes(n) {
+    if (n <= 2) return 0
+    return (n - 2) ** 3
+}
