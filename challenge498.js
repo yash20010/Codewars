206f4f37cacae31e46852537339fd5a3198f2c0b
throne.yash@gmail.com <throne.yash@gmail.com>
2021-06-15 16:34:38 -0600

    Adding more katas

---
+++ challenge498.js
@@ -0,0 +1,17 @@
+// Problem
+// Complete the function that takes an odd integer (0 < n < 1000000) which is the difference between two consecutive perfect squares, and return these squares as a string in the format "bigger-smaller"
+
+// Examples
+// 9  -->  "25-16"
+// 5  -->  "9-4"
+// 7  -->  "16-9"
+
+const findSquares = (num) => {
+    let max = 1
+    let min = 0
+    while (max ** 2 - min ** 2 != num) {
+        max++
+        min++
+    }
+    return `${max ** 2}-${min ** 2}`
+}
