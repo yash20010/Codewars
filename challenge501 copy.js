e27adbaa017e991aaf12a577019db4e040d88654
throne.yash@gmail.com <throne.yash@gmail.com>
2021-06-17 15:57:52 -0600

    Adding more Katas

---
+++ challenge501 copy.js
@@ -0,0 +1,23 @@
+// Task.
+
+// Calculate area of given triangle. Create a function t_area that will take a string which will represent triangle, find area of the triangle, one space will be equal to one length unit. The smallest triangle will have one length unit.
+
+// Hints
+
+// Ignore dots.
+
+// Example:
+
+// .
+// .      .
+// .      .       .      ---> should return 2.0
+
+// .
+// .      .
+// .      .       .
+// .      .       .      .      ---> should return 4.5
+
+function tArea(tStr) {
+    let arr = tStr.split('\n').slice(1, -1)
+    return 0.5 * (arr.length - 1) * ((arr.slice(-1)[0].length - 1) / 2)
+}
