e433ce6d4459849ca7e978ab98e0234fb8ae3c99
throne.yash@gmail.com <throne.yash@gmail.com>
2021-06-18 16:28:10 -0600

    Adding more Katas

--- challenge503.js
+++ challenge503.js
@@ -1,23 +1,32 @@
-// Task.
+// Overview
+// Many people know that Apple uses the letter "i" in almost all of its devices to emphasize its personality.
 
-// Calculate area of given triangle. Create a function t_area that will take a string which will represent triangle, find area of the triangle, one space will be equal to one length unit. The smallest triangle will have one length unit.
+// And so John, a programmer at Apple, was given the task of making a program that would add that letter to every word. Let's help him do it, too.
 
-// Hints
+// Task:
+// Your task is to make a function that takes the value of word and returns it with an "i" at the beginning of the word. For example we get the word "Phone", so we must return "iPhone". But we have a few rules:
 
-// Ignore dots.
+// The word should not begin with the letter "I", for example Inspire.
+// The number of vowels should not be greater than or equal to the number of consonants, for example East or Peace. ("y" is considered a consonant)
+// The first letter should not be lowercase, for example road.
+// If the word does not meet the rules, we return "Invalid word".
 
-// Example:
-
-// .
-// .      .
-// .      .       .      ---> should return 2.0
-
-// .
-// .      .
-// .      .       .
-// .      .       .      .      ---> should return 4.5
-
-function tArea(tStr) {
-    let arr = tStr.split('\n').slice(1, -1)
-    return 0.5 * (arr.length - 1) * ((arr.slice(-1)[0].length - 1) / 2)
+function i(word) {
+    if (
+        !word ||
+        word[0].toLowerCase() === 'i' ||
+        word[0] === word[0].toLowerCase()
+    ) {
+        return 'Invalid word'
+    } else if (
+        word.split('').filter((l) => 'aeiou'.includes(l.toLowerCase()))
+            .length >=
+        word.length -
+            word.split('').filter((l) => 'aeiou'.includes(l.toLowerCase()))
+                .length
+    ) {
+        return 'Invalid word'
+    } else {
+        return `i${word}`
+    }
 }
---
+++ challenge504.js
@@ -0,0 +1,35 @@
+// Sort the Vowels!
+// In this kata, we want to sort the vowels in a special format.
+
+// Task
+// Write a function which takes a input string s and return a string in the following way:
+
+//                   C|                          R|
+//                   |O                          n|
+//                   D|                          d|
+//    "Codewars" =>  |E       "Rnd Te5T"  =>      |
+//                   W|                          T|
+//                   |A                          |e
+//                   R|                          5|
+//                   S|                          T|
+
+// Notes:
+
+// List all the Vowels on the right side of |
+// List every character except Vowels on the left side of |
+// Case doesn't matter
+// Each line is seperated with \n
+// Invalid input ( undefined / null / integer ) should return an empty string
+
+function sortVowels(s) {
+    if (!s || typeof s === 'number') return ''
+    let arr = []
+    for (let i = 0; i < s.length; i++) {
+        if ('aeiou'.includes(s[i].toLowerCase())) {
+            arr.push(`|${s[i]}`)
+        } else {
+            arr.push(`${s[i]}|`)
+        }
+    }
+    return arr.join('\n')
+}
