d09f9560d12c9c6e786ce337b8b31c60602e86f1
throne.yash@gmail.com <throne.yash@gmail.com>
2021-07-01 15:24:33 -0600

    Adding more katas

---
+++ challenge543.js
@@ -0,0 +1,14 @@
+// ###Lucky number
+
+// Write a function to find if a number is lucky or not. If the sum of all digits is 0 or multiple of 9 then the number is lucky.
+
+// 1892376 => 1+8+9+2+3+7+6 = 36. 36 is divisible by 9, hence number is lucky.
+
+// Function will return true for lucky numbers and false for others.
+
+function isLucky(n) {
+  let sum = [...String(n)].reduce((a, c) => +a + +c)
+  return sum === 0 || sum % 9 === 0
+}
+
+// return n%9 === 0 (as the sum of all digits of a number divisible by 9 is also divisible by 9)
